cece91b18790479e42f269b46b41b692c4d6d057
Harmanen <hurmanen@gmail.com>
2023-10-12 10:53:40 +0300

    Implement initial grid layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,26 @@
-<div style="text-align: center; padding: 2em;">Root page here!</div>
+<script>
+  import { Card } from '$components';
+</script>
+
+<div class="grid-container">
+  <!-- TMP, replace with real data when grid works -->
+  {#each [1, 2, 3, 4, 5, 6] as item}
+    <div class="grid-item">
+      <Card {item} />
+    </div>
+  {/each}
+</div>
+
+<style lang="scss">
+  .grid-container {
+    padding: var(--grid-padding);
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    grid-template-rows: repeat(2, 1fr);
+  }
+  .grid-item {
+    width: 360px;
+    height: 430px;
+    padding: var(--grid-padding);
+  }
+</style>
