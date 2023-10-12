5f453352a4a260b2956add2e0f59d4fb1bafbea5
Harmanen <hurmanen@gmail.com>
2023-10-12 15:29:06 +0300

    Improve grid responsitivity

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,8 +15,24 @@
   .grid-container {
     padding: var(--grid-padding);
     display: grid;
+    justify-items: center;
+
+    // Default 5 columns
     grid-template-columns: repeat(5, 1fr);
-    grid-template-rows: repeat(2, 1fr);
+
+    // Decrease amount of columns based on the viewport width
+    @media screen and (max-width: '1915px') {
+      grid-template-columns: repeat(4, 1fr);
+    }
+    @media screen and (max-width: '1530px') {
+      grid-template-columns: repeat(3, 1fr);
+    }
+    @media screen and (max-width: '1160px') {
+      grid-template-columns: repeat(2, 1fr);
+    }
+    @media screen and (max-width: '800px') {
+      grid-template-columns: repeat(1, 1fr);
+    }
   }
   .grid-item {
     width: 360px;
