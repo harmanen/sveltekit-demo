f910887fb0a691028211fd505f750198e503bf83
Harmanen <hurmanen@gmail.com>
2023-10-12 10:53:24 +0300

    Create initial Card component

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  export let item: number; // TMP
+</script>
+
+<div class="card-container">Card: {item}</div>
+
+<style lang="scss">
+  .card-container {
+    width: 100%;
+    height: 100%;
+    border: 2px solid var(--primary-js-on);
+    border-radius: 25px;
+    background-color: var(--dark-gray);
+    color: var(--text-color);
+    :global(html.no-js) & {
+      border-color: var(--primary-js-off);
+    }
+  }
+</style>
