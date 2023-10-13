feaae196048931e58f1dd7349233ccfbf1cb4926
Harmanen <hurmanen@gmail.com>
2023-10-13 19:41:49 +0300

    Fix a bug in links

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -20,7 +20,7 @@
   <div class="buttons">
     <Button
       element="a"
-      href="object/{item.titles.canonical}">View more!</Button
+      href="/object/{item.titles.canonical}">View more!</Button
     >
     <LinkToWiki
       urls={{
