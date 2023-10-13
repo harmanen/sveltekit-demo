f3f1686a7d052aac79b707b5513cdac002def183
Harmanen <hurmanen@gmail.com>
2023-10-13 11:45:55 +0300

    Implement pagination using JS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,47 @@
 <script lang="ts">
   import { Card } from '$components';
+  import Button from '$components/Button.svelte';
   import type { PageData } from './$types';
 
   export let data: PageData;
+
+  let showLoadMoreButton = true;
+
+  // Initially load one row (on full width screen)
+  let selectedData = data.mockData.slice(0, 5);
+
+  let i = 5;
+
+  const handleLoadMoreObjects = () => {
+    if (i + 5 <= data.mockData.length) {
+      // Load a full row of 5 more items
+      selectedData = [...selectedData, ...data.mockData.slice(i, i + 5)];
+      i += 5;
+    } else {
+      // Load the rest of items and hide the load more button
+      selectedData = [...selectedData, ...data.mockData.slice(i)];
+      showLoadMoreButton = false;
+    }
+  };
 </script>
 
 <div class="grid-container">
   <!-- TMP, replace with real data when grid works -->
-  {#each data.mockData as item}
+  {#each selectedData as item}
     <div class="grid-item">
       <Card {item} />
     </div>
   {/each}
 </div>
+
+{#if showLoadMoreButton}
+  <div class="load-more-container">
+    <Button
+      element="button"
+      on:click={handleLoadMoreObjects}>Load more objects</Button
+    >
+  </div>
+{/if}
 
 <style lang="scss">
   .grid-container {
@@ -45,5 +74,11 @@
   }
   .grid-item:hover {
     transform: scale(1.05);
+  }
+  .load-more-container {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    padding: 10px 0;
   }
 </style>
