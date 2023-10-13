06e35a4c95667b22d747085c293a2dc598cd8f3e
Harmanen <hurmanen@gmail.com>
2023-10-13 19:09:36 +0300

    Move no-JS navigation from layout to page

--- src/routes/objects/page/[id]/+layout.svelte
+++ src/routes/objects/page/[id]/+layout.svelte
@@ -1,32 +1,5 @@
-<script>
-  import { page } from '$app/stores';
-  import Button from '$components/Button.svelte';
-</script>
-
 <div class="objects-page-layout">
   <slot />
-  <div class="page-navigation-buttons">
-    {#if Number($page.params.id) - 1 > 0}
-      <div class="navigation-button-container">
-        <Button
-          element="a"
-          variant="navigation"
-          href={`/objects/page/${Number($page.params.id) - 1}`}
-          >Previous page</Button
-        >
-      </div>
-    {/if}
-    {#if $page.params.id < $page.data.lastPage}
-      <div class="navigation-button-container">
-        <Button
-          element="a"
-          variant="navigation"
-          href={`/objects/page/${Number($page.params.id) + 1}`}
-          >Next page</Button
-        >
-      </div>
-    {/if}
-  </div>
 </div>
 
 <style lang="scss">
@@ -34,13 +7,5 @@
     @media screen and (max-width: '800px') {
       padding: 20px;
     }
-  }
-  .page-navigation-buttons {
-    display: flex;
-    justify-content: center;
-    width: 100%;
-  }
-  .navigation-button-container {
-    padding: 20px 10px;
   }
 </style>
--- src/routes/objects/page/[id]/+page.svelte
+++ src/routes/objects/page/[id]/+page.svelte
@@ -1,8 +1,42 @@
 <script lang="ts">
   import ObjectsGrid from '$components/ObjectsGrid.svelte';
   import type { PageData } from './$types';
+  import { page } from '$app/stores';
+  import Button from '$components/Button.svelte';
 
   export let data: PageData;
 </script>
 
 <ObjectsGrid data={data.mockData} />
+<div class="page-navigation-buttons">
+  {#if Number($page.params.id) - 1 > 0}
+    <div class="navigation-button-container">
+      <Button
+        element="a"
+        variant="navigation"
+        href={`/objects/page/${Number($page.params.id) - 1}`}
+        >Previous page</Button
+      >
+    </div>
+  {/if}
+  {#if $page.params.id < $page.data.lastPage}
+    <div class="navigation-button-container">
+      <Button
+        element="a"
+        variant="navigation"
+        href={`/objects/page/${Number($page.params.id) + 1}`}>Next page</Button
+      >
+    </div>
+  {/if}
+</div>
+
+<style lang="scss">
+  .page-navigation-buttons {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+  }
+  .navigation-button-container {
+    padding: 20px 10px;
+  }
+</style>
